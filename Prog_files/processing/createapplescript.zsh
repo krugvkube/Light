2782e04040c2cb9012cf69669ec176b8d519b788
krugvkube <antoncygankov6@gmail.com>
2025-08-23 17:20:37 +0300

    Update createapplescript.zsh

--- Prog_files/processing/createapplescript.zsh
+++ Prog_files/processing/createapplescript.zsh
@@ -1,1 +1,19 @@
+#!/bin/zsh
 
+# Команда
+APPLESCRIPT_CONTENT='do shell script "python3 " & quoted form of "___"'
+
+# Создаем папку если нет
+mkdir -p ~/Library/Script\ Libraries/
+
+# Временный файл
+TEMP_FILE=$(mktemp)
+echo "$APPLESCRIPT_CONTENT" > "$TEMP_FILE.applescript"
+
+# Компилируем
+osacompile -o ~/Library/Script\ Libraries/PythonRunner.scpt "$TEMP_FILE.applescript"
+
+# Удаляем временный файл
+rm "$TEMP_FILE.applescript"
+
+echo "AppleScript создан в ~/Library/Script Libraries/"
